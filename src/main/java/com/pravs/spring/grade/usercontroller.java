acdaeb3faeef1c4759083475eb25767d79e0b124
Praveen Sharma <praveenksharma@gmail.com>
2018-06-02 13:28:09 -0400

    Rest endpoint using parameter.

--- src/main/java/com/pravs/spring/grade/usercontroller.java
+++ src/main/java/com/pravs/spring/grade/usercontroller.java
@@ -1,13 +1,13 @@
 package com.pravs.spring.grade;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class usercontroller {
     @RequestMapping("user")
-    public String user() {
-        return "Welcome to user controller!!!";
+    public String user(@RequestParam(value = "name", defaultValue = "User1") String name) {
+        return "Hello User " + name;
     }
 }
