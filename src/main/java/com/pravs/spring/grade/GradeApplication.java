e79db1182250baf0939e8d6196a830ff8f9a1877
Praveen Sharma <praveenksharma@gmail.com>
2018-06-11 22:25:28 -0400

    Now doing DI via xml

--- src/main/java/com/pravs/spring/grade/GradeApplication.java
+++ src/main/java/com/pravs/spring/grade/GradeApplication.java
@@ -2,11 +2,8 @@ package com.pravs.spring.grade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.pravs.spring.grade.models.user;
-import org.joda.time.LocalTime;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-
 
 @SpringBootApplication
 @ComponentScan(excludeFilters = {
@@ -14,13 +11,6 @@ import org.springframework.context.annotation.FilterType;
 public class GradeApplication {
 
 	public static void main(String[] args) {
-		user User = new user(1, "Bill", "Gates");
-		System.out.println("User's id is: " + User.getId() + " First and last name is: " + User.getFirstName() + " " + User.getLastName());
-
-		System.out.println("User's full name is " + User.getFullName());
-
-		LocalTime currentTime = new LocalTime();
-		System.out.println("The current local time is: " + currentTime);
 		SpringApplication.run(GradeApplication.class, args);
 	}
 }
--- src/main/java/com/pravs/spring/grade/JavaConfig/CDPlayerConfig.java
+++ src/main/java/com/pravs/spring/grade/JavaConfig/CDPlayerConfig.java
@@ -3,16 +3,18 @@ package com.pravs.spring.grade.JavaConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.pravs.spring.grade.models.soundsystem.*;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
+@ImportResource({"classpath:beans.xml"})
 public class CDPlayerConfig {
-    @Bean
-    public guru guru() {
-        return new guru();
-    }
-
-    @Bean
-    public CDPlayer cdPlayer() {
-        return new CDPlayer(guru());
-    }
+//    @Bean
+//    public guru guru() {
+//        return new guru();
+//    }
+//
+//    @Bean
+//    public CDPlayer cdPlayer() {
+//        return new CDPlayer(guru());
+//    }
 }
