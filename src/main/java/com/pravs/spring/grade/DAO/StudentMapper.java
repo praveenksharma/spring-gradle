5e2488877e5b916c1a542d093c3725e5ed48c5b5
Praveen Sharma <praveenksharma@gmail.com>
2018-06-12 23:13:03 -0400

    Trying spring jdbc

---
+++ src/main/java/com/pravs/spring/grade/DAO/StudentMapper.java
@@ -0,0 +1,17 @@
+package com.pravs.spring.grade.DAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+import com.pravs.spring.grade.models.Student;
+
+public class StudentMapper implements RowMapper<Student> {
+    public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Student student = new Student();
+        student.setId(rs.getInt("id"));
+        student.setName(rs.getString("name"));
+        student.setAge(rs.getInt("age"));
+
+        return student;
+    }
+}
